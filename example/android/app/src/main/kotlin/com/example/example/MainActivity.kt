45c461f8ec4ae380a6627bd9ed880dad8b359b53
Oussama Maatallah <Oussama.Maatallah@expensya.com>
2023-03-08 15:58:58 +0100

    fixed wide buttons issue

--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.slider_button_example
+package com.example.example
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/src/slider.dart
+++ lib/src/slider.dart
@@ -15,6 +15,9 @@ class SliderButton extends StatefulWidget {
   final double width;
   final double? buttonSize;
 
+  /// Use this parameter in case if your slider button is wide and not squared.
+  final double? buttonWidth;
+
   ///Use it to define a color of widget.
   final Color backgroundColor;
   final Color baseColor;
@@ -54,6 +57,7 @@ class SliderButton extends StatefulWidget {
     this.shimmer = true,
     this.height = 70,
     this.buttonSize,
+    this.buttonWidth,
     this.width = 250,
     this.alignLabel = const Alignment(0.6, 0),
     this.backgroundColor = const Color(0xffe0e0e0),
@@ -162,7 +166,7 @@ class _SliderButtonState extends State<SliderButton> {
                       }
                     },
                     child: Container(
-                      width: widget.width - widget.height,
+                      width: widget.width - (widget.buttonWidth ?? widget.height),
                       height: widget.height,
                       alignment: Alignment.centerLeft,
                       padding: EdgeInsets.only(
